c07e6efa9ca7e7614d95f68b447fd6508c335d15
Vishal Patel <85611565+ZGTvishal@users.noreply.github.com>
2026-06-18 11:40:44 +0100

    basic dashboard and views

--- Applications/views.py
+++ Applications/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_view(request):
-    return HttpResponse("Job Tracker Home")
+    return render(request, 'Applications/home.html')
