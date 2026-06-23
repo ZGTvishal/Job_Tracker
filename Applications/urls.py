5a64c236356c786a7049e1cfc86bcec8c4abd0a2
Vishal Patel <85611565+ZGTvishal@users.noreply.github.com>
2026-06-23 17:20:40 +0100

    API integration complete

--- Applications/urls.py
+++ Applications/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
-    path('connect-gmail', views.connect_gmail, name='connect-gmail')
+    path('connect-gmail', views.connect_gmail, name='connect-gmail'),
+    path('oauth2callback/', views.oauth2callback, name='oauth2callback'),
+
 ]
--- Applications/views.py
+++ Applications/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 import urllib.parse
 import os
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -30,3 +31,49 @@ def connect_gmail(request):
     url_args = urllib.parse.urlencode(params)
     auth_url = f"{google_auth_base_url}?{url_args}"
     return redirect(auth_url)
+
+
+def oauth2callback(request):
+    code = request.GET.get('code')
+    if not code:
+        return HttpResponse("Error", status = 400)
+    
+    token_url = "https://oauth2.googleapis.com/token"
+
+    payload = {
+        'code': code,
+        'client_id': os.environ.get('GOOGLE_CLIENT_ID'),
+        'client_secret': os.environ.get('GOOGLE_CLIENT_SECRET'),
+        'redirect_uri': "http://127.0.0.1:8000/oauth2callback/",
+        'grant_type': 'authorization_code',
+
+    }
+    response = requests.post(token_url, data = payload)
+
+    if response.status_code != 200:
+        # 
+        # return JsonResponse(token_data)
+        return HttpResponse(f"Failure Occured{response.status_code}", status= response.status_code)
+   
+   
+    token_data = response.json()
+    access_token = token_data.get('access_token')
+
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'Accept': 'application/json',
+    }
+    gmail_uri = "https://gmail.googleapis.com/gmail/v1/users/me/messages?maxResults=10"
+
+    gmail_response = requests.get(gmail_uri, headers=headers)
+
+    if gmail_response.status_code == 200:
+        email_list = gmail_response.json()
+        message = email_list.get('messages', [])
+        first_message_id = message[0]['id']
+        msg_url = f"https://gmail.googleapis.com/gmail/v1/users/me/messages/{first_message_id}"
+        msg_response = requests.get(msg_url, headers=headers)
+        if msg_response.status_code == 200:
+            return JsonResponse(msg_response.json())
+    else:
+        return HttpResponse("Oops something is not working!")
